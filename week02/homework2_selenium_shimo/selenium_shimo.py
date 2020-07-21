0f7489fbc879b826bbd76485fd08dac502419524
ericksonkop <875859658@qq.com>
2020-07-22 07:54:39 +0800

    checked

--- week02/homework2_selenium_shimo/selenium_shimo.py
+++ week02/homework2_selenium_shimo/selenium_shimo.py
@@ -11,8 +11,8 @@ driver = webdriver.Chrome('I:/chromedriver_win32/chromedriver.exe')
 driver.get('https://shimo.im/login?from=home')
 driver.implicitly_wait(10)
 try:
-    driver.find_element_by_css_selector("input[type='text']").send_keys('13628639530')
-    driver.find_element_by_css_selector("input[type='password']").send_keys('54kopallmylife')
+    driver.find_element_by_css_selector("input[type='text']").send_keys('136XXXXXXXX')
+    driver.find_element_by_css_selector("input[type='password']").send_keys('XXXXXXXXXXX')
     driver.find_element_by_css_selector("button[type='black']").click()
     time.sleep(3)
     assert '石墨文档' in driver.title
